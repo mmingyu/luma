bef05e3344e0e95a328b8241a18c4db6b7b4f012
mingyu <alsrb4298@naver.com>
2026-09-08 00:00:51 +0900

    Add Swift package manifest

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Luma",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "Luma", targets: ["Luma"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "Luma",
+            path: "Sources/Luma"
+        )
+    ]
+)
